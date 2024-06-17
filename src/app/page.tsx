95eb54639573457cf49d7651b3e92da18a97fa5a
elianabean <ewanginspiration@gmail.com>
2024-06-17 13:19:44 -0400

    landing page sections 2 and 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,6 +95,75 @@ export default function Home() {
             />
           
         </div>
+
+        <div className="flex flex-col h-[100vh] justify-start pl-36 relative w-[100vw] items-end ">
+          <div className="absolute bottom-[504px] w-[calc(100%-144px)] h-[238px]">
+              <Image src="/images/rectangle-3174.svg" alt="background" fill style={{objectFit: "cover"}}/>
+          </div>
+          <div className="absolute bottom-0 w-[calc(100%-144px)] h-[504px] ">
+              <Image src="/images/rectangle-3175.svg" alt="background" fill style={{objectFit: "cover"}} />
+          </div>
+            
+          <Image
+              src="/images/image-31.png"
+              alt="ai credit valuations"
+              className="absolute left-[242px] top-[154px] z-0"
+              width={541} height={339}
+            />
+            <Image
+              src="/images/image-30.png"
+              alt="ai credit valuations"
+              className="absolute left-[416px] top-[386px] z-0"
+              width={544} height={350}
+            />
+
+          <div className="z-10 flex flex-col items-start mr-[256px] mt-[128px]">
+            <p className="w-[571.359px] shrink-0 text-black font-montserrat text-5xl font-normal leading-[50px] capitalize"><span className="text-[#538DFF] font-semibold">AI Credit valuations</span> That Leaves No Room for Guesswork
+            </p>
+
+            <p className="w-[529px] text-black font-inter text-[23px] font-medium leading-[150%] mt-[50px]">Our generative AI doesn't just answer questions — Intellect.ai analyses user inefficiencies to evaluate trends and inform users on opportunities they may overlook. This reduces asymmetric information and allows users to make efficient and informed decisions.</p>
+            
+          </div>          
+        </div>
+
+        <div className="flex flex-col h-[100vh] justify-center pl-[242px] relative">
+          <p className="w-[638px] text-black font-montserrat text-5xl font-semibold leading-[50px] capitalize">Say Goodbye to <span className="text-[#538DFF]">Risky & Inefficient</span> Money Management </p>
+          <Image
+              src="/images/ellipse-35.png"
+              alt="background"
+              className="absolute right-0 z-0"
+              width={1391.834} height={1059.03}
+            />
+            
+          <div className="z-10 flex flex-col items-start">
+            <p className="w-[600px] text-black font-inter text-[22px] font-semibold leading-[150%] mb-[32px] mt-[50px]">While individuals may keep their finances afloat, building lasting wealth demands a deeper level of insight.</p>
+            <p className="w-[600px] text-black font-inter text-[22px] font-semibold leading-[150%] mb-[32px]">Intellect.ai <span className="text-[#538DFF]">transcends</span> conventional financial advice. It provides an personalized analysis encompassing your account's strengths, areas for improvement, tailored investment strategies, budgeting tactics, and more.</p>
+            <p className="w-[600px] text-black font-inter text-[22px] font-bold leading-[150%]">Empower Your Finances: Not Just Managing, but Mastering</p>
+
+            <Button variant="shadow" radius="full" color="primary" className="font-inter px-[20px] py-[24px] [background:linear-gradient(5deg,#588FEF_26.3%,#8FD7FF_86.4%)] mt-[32px]" endContent={<Image
+              src="/icons/lightning-bolt.svg"
+              alt="background"
+              className="fill-white"
+              width={20} height={20}
+            />}>Continue Generation</Button>
+          </div>
+
+          <div className="absolute top-1/2 translate-y-[-50%] right-[147px] ">
+            <Image
+              src="/images/image-34.png"
+              alt="chat"
+              width={892} height={501}
+            />
+            <Image
+              src="/images/image-35.png"
+              alt="chat" className="absolute top-[350px] right-[50px]"
+              width={427} height={280}
+            />
+            
+          </div>
+          
+          
+        </div>
       </div>
     </div>
   );
