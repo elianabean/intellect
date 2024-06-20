5ff88e3076d76f2ca061dc0d56a51bc26e2af169
elianabean <ewanginspiration@gmail.com>
2024-06-20 14:23:20 -0400

    dynamic sidebar highlighting

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,17 +1,25 @@
+'use client'
+
 import Image from "next/image";
 import { NavLink } from "react-router-dom";
 import Link from 'next/link'
+import { usePathname } from "next/navigation";
+
 
 export default function Sidebar() {
+  const currentPath = usePathname();
+
     return (
       <div className="3xl:w-36 h-full bg-white flex flex-col items-center justify-start fixed left-0 px-[20px] pt-[67px] z-50">
-        <p className="text-[#588FEF] font-inter text-xl italic font-black leading-[150%]">Intellect.ai</p>
-        
         <Link href="/">
+        <p className="text-[#588FEF] font-inter text-xl italic font-black leading-[150%]">Intellect.ai</p>
+        </Link>
+
+        <Link href="/" >
         <Image
-              src="/icons/iconly-bold-home.svg"
+              src={currentPath === "/" ? "/icons/iconly-bold-home-blue.svg" : "/icons/iconly-bold-home.svg"}
               alt="Home"
-              className="dark:invert mt-[108px]"
+              className="dark:invert mt-[108px] "
               width={20}
               height={20}
             />
@@ -19,16 +27,16 @@ export default function Sidebar() {
         
         <Link href="/chat">
         <Image
-              src="/icons/sp-ai.svg"
+              src={currentPath === "/chat" ? "/icons/sp-ai-blue.svg" : "/icons/sp-ai.svg"}
               alt="Chat"
               className="dark:invert mt-[40px] border-0"
-              width={63}
-              height={43}
+              width={currentPath === "/chat" ? 59 : 63}
+              height={currentPath === "/chat" ? 36 : 43}
             />
         </Link>
         <Link href="/settings">
         <Image
-              src="/icons/iconly-bold-setting.svg"
+              src={currentPath === "/settings" ? "/icons/iconly-bold-setting-blue.svg" : "/icons/iconly-bold-setting.svg"}
               alt="Settings"
               className="dark:invert mt-[40px]"
               width={24}
