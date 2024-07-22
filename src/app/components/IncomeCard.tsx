271ba8c9bc2cace4a4906ed311dd74baf7e99c8f
Shoji Watanabe <shoji.watanabe2@gmail.com>
2024-07-21 23:36:10 -0400

    income card
    placeholder

---
+++ src/app/components/IncomeCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function IncomeCard() {
+  return (
+    <div className="bg-white shadow-md p-4 flex flex-row justify-between rounded-lg h-[200px] w-[400px]">
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-[#3A4F38] font-raleway text-[20px]">Your Financial Battle: Income VS Expenses</p>  
+      </div>
+    </div>
+  )
+}
