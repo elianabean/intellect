7a68bdbba19950a6738708e1c1a5788f944096b9
Shoji Watanabe <shoji.watanabe2@gmail.com>
2024-07-21 23:41:17 -0400

    budgeting card
    placeholder

---
+++ src/app/components/BudgetingCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function BudgetingCard() {
+  return (
+    <div className="bg-white shadow-md p-4 flex flex-row justify-between rounded-lg h-[200px] w-[400px]">
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-[#3A4F38] font-raleway text-[21px]">Bring your thoughts to Life! Test out your Budgeting Scenarios</p>  
+      </div>
+    </div>
+  )
+}
