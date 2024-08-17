67527b8e03a3de8442620f20ab0192a0d061154f
MateoYa <mateo.e.yajure@gmail.com>
2024-08-17 04:40:18 -0600

    text styling adjustments

--- src/app/PersonalizedWallet/page.tsx
+++ src/app/PersonalizedWallet/page.tsx
@@ -65,27 +65,27 @@ export default function Homepage2() {
              */}
             <div className="relative ">
                 <PieChart data={dataPie}/>
-                <p className="absolute text-[#515251] text-2xl absolute top-[38%] left-[50%] translate-x-[-50%] translate-y-[-10%] w-[30%] text-center">Your Personalized Wallet</p>
+                <p className="absolute text-[#515251] text-2xl absolute top-[38%] left-[50%] translate-x-[-50%] translate-y-[-10%] w-[30%] text-center font-raleway font-bold ">Your Personalized Wallet</p>
             </div>
             <div className="absolute w-[247.88px] h-[30.05px] text-gray-500 top-[60%] text-[20px] translate-x-[-20%] translate-y-[150px] font-semibold font-['Inter'] leading-snug">
-            <div className="text-gray-500 top-[60%] text-[20px] leading-snug">KEY</div>
-            <div className="flex text-gray-500 text-[15px] leading-snug space-x-[10px]" style={{ marginTop: 5 }}>
+            <div className="text-gray-500 top-[60%] text-[20px] leading-snug font-inter font-semibold">KEY</div>
+            <div className="flex text-gray-500 text-[15px] leading-snug space-x-[10px] font-inter font-medium" style={{ marginTop: 5 }}>
               <CircleIcon className="text-[#96F996]"/>
               <div>Food</div>  
             </div>
-            <div className="flex text-gray-500 text-[15px] leading-snug space-x-[10px]" style={{ marginTop: 5 }}>
+            <div className="flex text-gray-500 text-[15px] leading-snug space-x-[10px] font-inter font-medium" style={{ marginTop: 5 }}>
               <CircleIcon className="text-[#C1E9FF]"/> 
               <div>Entertainment</div>  
             </div>
-            <div className="flex text-gray-500 text-[15px] leading-snug space-x-[10px]" style={{ marginTop: 5 }}>
+            <div className="flex text-gray-500 text-[15px] leading-snug space-x-[10px] font-inter font-medium" style={{ marginTop: 5 }}>
               <CircleIcon className="text-[#C0BBFF]"/>
               <div>Clubs</div> 
             </div>
-            <div className="flex text-gray-500 text-[15px] leading-snug space-x-[10px]" style={{ marginTop: 5 }}>
+            <div className="flex text-gray-500 text-[15px] leading-snug space-x-[10px] font-inter font-medium" style={{ marginTop: 5 }}>
               <CircleIcon className="text-[#BCFF87]"/>
               <div>Transportation </div>
             </div>
-            <div className="flex text-gray-500 text-[15px] leading-snug space-x-[10px]" style={{ marginTop: 5 }}>
+            <div className="flex text-gray-500 text-[15px] leading-snug space-x-[10px] font-inter font-medium" style={{ marginTop: 5 }}>
               <CircleIcon className="text-[#A5F1ED]"/>
               <div>Housing</div> 
             </div>
@@ -101,18 +101,20 @@ export default function Homepage2() {
             <PrecentageAndAbsRep label="Housing" realValue={50} maxValue={100} />
             </div>
             <div style={{ marginTop: 30 }} className="[background:rgba(252,251,255,0.80)] shadow-[5px_4px_10px_0px_rgba(0,0,0,0.25)] rounded-[10px] border-b border-solid border-b-[#516958]" >
-            <MyRadarChart data={[
-                { name: 'Food', x: 400 },
-                { name: 'Entertainment', x: 100 },
-                { name: 'Transportation', x: 200},
-                { name: 'Clubs', x: 100 },
-                { name: "Housing", x: 950}
-              ]}/>
-            <CircleIcon className="text-[#A5F1ED] translate-x-[5px] translate-y-[-205px]"/>
-            <CircleIcon className="text-[#C0BBFF] translate-x-[32px] translate-y-[-50px]"/>
-            <CircleIcon className="text-[#BCFF87] translate-x-[173px] translate-y-[-50px]"/>
-            <CircleIcon className="text-[#C1E9FF] translate-x-[195px] translate-y-[-205px]"/>
-            <CircleIcon className="text-[#96F996] translate-x-[41px] translate-y-[-298px]"/>
+            <div className="translate-y-[10px]">
+              <MyRadarChart data={[
+                  { name: 'Food', x: 400 },
+                  { name: 'Entertainment', x: 100 },
+                  { name: 'Transportation', x: 200},
+                  { name: 'Clubs', x: 100 },
+                  { name: "Housing", x: 950}
+                ]}/>
+              <CircleIcon className="text-[#A5F1ED] translate-x-[0px] translate-y-[-210px]"/>
+              <CircleIcon className="text-[#C0BBFF] translate-x-[28px] translate-y-[-45px]"/>
+              <CircleIcon className="text-[#BCFF87] translate-x-[177px] translate-y-[-45px]"/>
+              <CircleIcon className="text-[#C1E9FF] translate-x-[200px] translate-y-[-212px]"/>
+              <CircleIcon className="text-[#96F996] translate-x-[41px] translate-y-[-306px]"/>
+            </div>
 
             </div>
         </div>
@@ -146,16 +148,23 @@ export default function Homepage2() {
                     value={100 -props.realValue/props.maxValue*100}
                     text={`${props.realValue/props.maxValue*100}%`}
                     styles={buildStyles({
+                      textSize: 22,
                       rotation: 0.5 + (17-props.realValue/props.maxValue - 5 / 100) / 2,
-                      trailColor: "black",
+                      trailColor: "#C4C2C2",
                       pathColor: color
                     })}
                   />
-                  <div style={{width: "50%", alignContent: "center", paddingLeft: "10%"} } className=" translate-y-[10px]">
+                  <div style={{width: "50%", alignContent: "center", paddingLeft: "10%"} } className=" translate-y-[25px]">
                   <div style={{width: '100%', height: '100%', position: 'relative'}}>
                   <div style={{width: 84.41, height: 9, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 50, border: (100-props.realValue/props.maxValue*100<=33) ?"#FA224B 1px solid": (100-props.realValue/props.maxValue*100<=66)? "#FFE073 1px solid": "#96F996 1px solid"}} />
                   <div style={{width:  84.41 * (1 - props.realValue/props.maxValue), height: 9, left: 0.41, top: 0, position: 'absolute', background: (100-props.realValue/props.maxValue*100<=33) ?"#FA224B": (100-props.realValue/props.maxValue*100<=66)? "#FFE073": "#96F996", borderRadius: 50}} />
-                  <h3 className="h5 text-center text-s text-black  w-[200%] translate-y-[10px]">${props.realValue} spent</h3>
+                  
+                    <h3 className="h5 text-center font-lato font-semibold text-[#6B7280]  w-[200%] translate-y-[10px]">
+                    <span className="text-base">$</span>
+                    <span className="text-lg">{props.realValue}</span>
+                    <span className="text-sm"> spent</span>
+
+                    </h3>
                   </div>
                     
                   </div>
